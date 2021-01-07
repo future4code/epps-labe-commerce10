13faab81d12fe93c48a076eb86af7527544ffdce
DanielRatti <danielpr55rr@gmai.com>
2021-01-07 13:48:04 -0300

    mundanças no carrinho e adicionado o botao carrinho

--- src/Components/Carrinho.js
+++ src/Components/Carrinho.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+
+const ContainerCarrinho = styled.div`
+  border: solid 2px black;
+`;
 
 class Carrinho extends React.Component {
-    render(){
-        return(
-            <Container>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <ContainerCarrinho>
+        <h2>Carrinho:</h2>
+        <p>Total:</p>
+      </ContainerCarrinho>
+    );
+  }
 }
+
+export default Carrinho;
